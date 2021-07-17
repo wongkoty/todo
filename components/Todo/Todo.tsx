d66b25cb8ff869135882017fe8e8121698d710d5
Koty Wong <wongkoty@gmail.com>
2021-07-16 23:30:43 -0400

    Batch updates to state

--- components/Todo/Todo.tsx
+++ components/Todo/Todo.tsx
@@ -3,12 +3,30 @@ import { format } from 'date-fns';
 
 import { ITodo } from '../../src/typings';
 
+type BackgroundColors = 'grey' | 'lightgreen' | 'pink';
+
 interface IProps {
   todo: ITodo;
 }
 
+interface PageState {
+  todo: ITodo;
+  backgroundColor: BackgroundColors;
+}
+
+const getBackgroundColor = (isComplete: boolean, dueDate: string | null): BackgroundColors => {
+  if (isComplete) return 'lightgreen';
+
+  if (dueDate && new Date(dueDate).getTime() < new Date().getTime()) return 'pink' 
+    
+  return 'grey';
+}
+
 export const Todo: FC<IProps> = ({ todo }) => {
-  const [isComplete, setIsComplete] = useState<boolean>(todo.isComplete);
+  const [pageState, setPageState] = useState<PageState>({
+    todo,
+    backgroundColor: getBackgroundColor(todo.isComplete, todo.dueDate)
+  });
 
   const updateIsCompletedTodo = async (id: string, isComplete: boolean) => {
     return fetch(`https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/patch/${id}`, {
@@ -23,12 +41,21 @@ export const Todo: FC<IProps> = ({ todo }) => {
     });
   }
 
-  const getBackgroundColor = (isComplete: boolean, dueDate: string | null) => {
-    if (isComplete) return 'lightgreen';
+  const handleChange = async () => {
+    const res = await updateIsCompletedTodo(pageState.todo.id, pageState.todo.isComplete);
+    const body = await res.json();
 
-    if (dueDate && new Date(dueDate).getTime() < new Date().getTime()) return 'pink' 
-      
-    return 'grey';
+    if (body.status === 'success') {
+      setPageState(prev => {
+        return {
+          todo: {
+            ...prev.todo,
+            isComplete: !prev.todo.isComplete,
+          },
+          backgroundColor: getBackgroundColor(!prev.todo.isComplete, pageState.todo.dueDate)
+        }
+      })
+    }
   }
 
   return (
@@ -36,7 +63,7 @@ export const Todo: FC<IProps> = ({ todo }) => {
       style={{
         justifyContent: 'space-between',
         display: 'flex',
-        backgroundColor: getBackgroundColor(todo.isComplete, todo.dueDate),
+        backgroundColor: pageState.backgroundColor,
         margin: '24px 0',
         padding: '24px 0'
       }}
@@ -46,26 +73,19 @@ export const Todo: FC<IProps> = ({ todo }) => {
       >
         <input 
           type='checkbox'
-          checked={isComplete}
-          onChange={async () => {
-            const res = await updateIsCompletedTodo(todo.id, isComplete);
-            const body = await res.json();
-
-            if (body.status === 'success') {
-              setIsComplete(prev => !prev);
-            }
-          }}
+          checked={pageState.todo.isComplete}
+          onChange={async () => await handleChange()}
         />
       </div>
       <div
-        style={{ flexBasis: '20% '}}
+        style={{ flexBasis: '65% '}}
       >
-        <span style={{ textDecorationLine: todo.isComplete ? 'line-through' : undefined }}>{todo.description}</span>
+        <span style={{ textDecorationLine: pageState.todo.isComplete ? 'line-through' : undefined }}>{pageState.todo.description}</span>
       </div>
       <div
-        style={{ flexBasis: '65%', alignSelf: 'center' }}  
+        style={{ flexBasis: '20%', alignSelf: 'center' }}  
       >
-        {todo.dueDate ? format(new Date(todo.dueDate), 'MM/dd/yyyy') : ''}
+        {pageState.todo.dueDate ? format(new Date(pageState.todo.dueDate), 'MM/dd/yyyy') : ''}
       </div>
     </div>
   )
